4bf049d208bd8abeb8f27c84d2a8708484dd6e1e
Israfil666 <113192535+Israfil666@users.noreply.github.com>
2024-01-23 10:09:50 -0800

    bug fix

--- script/index.js
+++ script/index.js
@@ -2,134 +2,119 @@ const name = document.querySelector("#name");
 const surname = document.querySelector("#surname");
 const tel = document.querySelector("#tel");
 const email = document.querySelector("#email");
-const tBody = document.querySelector("tbody")
-const saveBtn = document.querySelector("#save-button")
+const tBody = document.querySelector("tbody");
+const saveBtn = document.querySelector("#save-button");
 const modifayButton = document.querySelector("#edit-edit");
 const xButton = document.querySelector("#x");
 let userDataObj = [];
 
-
-
-saveBtn.addEventListener("click",(e)=>{
-    e.preventDefault()
-    newUser()
-    createUserTable(userDataObj)
-    editInputImport()
-})
-
+saveBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  newUser();
+  createUserTable(userDataObj);
+  editInputImport();
+});
 
 function newUser() {
-    // dom - dan gələn (name.value)-sunu göturəm yeni array yaradiram ve (userdataobj əlavə edirəm) (trim())- funksiyasi (istifadəci tərəfindən göndərilən bosluqlari doldurur)
-    if (name.value.length == 0) {
-        alert("name bolmesini doldur")
-        return
-    }
-    else 
-    if (surname.value.length == 0) {
-        alert("Surname bolmesini doldur")
-        return
-    }
-    if (tel.value.length == 0) {
-        alert("Tel bolmesini doldur")
-        return
-    }
-    if (email.value.length == 0) {
-        alert("Email bolmesini doldur")
-        return
-    }
-    
-   let user={
-    name:name.value.trim(),
-    surname:surname.value.trim(),
-    tel:tel.value.trim(),
-    email:email.value.trim()
-}
-    userDataObj.push(user)
+  // dom - dan gələn (name.value)-sunu göturəm yeni array yaradiram ve (userdataobj əlavə edirəm) (trim())- funksiyasi (istifadəci tərəfindən göndərilən bosluqlari doldurur)
+  if (name.value.length == 0) {
+    alert("name bolmesini doldur");
+    return;
+  } else if (surname.value.length == 0) {
+    alert("Surname bolmesini doldur");
+    return;
+  }
+  if (tel.value.length == 0) {
+    alert("Tel bolmesini doldur");
+    return;
+  }
+  if (email.value.length == 0) {
+    alert("Email bolmesini doldur");
+    return;
+  }
+
+  let user = {
+    name: name.value.trim(),
+    surname: surname.value.trim(),
+    tel: tel.value.trim(),
+    email: email.value.trim(),
+  };
+  userDataObj.push(user);
 }
 function createUserTable(items) {
-    tBody.innerHTML=""
-    items.forEach((item) => {
-        const tr =document.createElement("tr")
-        tr.classList="hi"
-        tr.innerHTML=`
+  tBody.innerHTML = "";
+  items.forEach((item) => {
+    const tr = document.createElement("tr");
+    tr.classList = "hi";
+    tr.innerHTML = `
         <td>${item.name}</td>
         <td>${item.surname}</td>
         <td>${item.tel}</td>
         <td>${item.email}</td>
         <td><button type="button" class="remove">Remove</button> <button type="button" class="edit">edit</button></td>
-        `
-        tBody.append(tr)
-    });
-    inputReset()
-    remove()
-
+        `;
+    tBody.append(tr);
+  });
+  inputReset();
+  remove();
 }
 function inputReset() {
-    name.value="",
-    surname.value="",
-    tel.value="",
-    email.value=""
+  (name.value = ""), (surname.value = ""), (tel.value = ""), (email.value = "");
 }
 let index;
 function editInputImport() {
-    const editsButtons = document.querySelectorAll(".edit")
-    editsButtons.forEach((item,indexBtn)=>{
-        item.addEventListener("click",()=>{
-           index=indexBtn
-           name.value=userDataObj[indexBtn].name
-           surname.value=userDataObj[indexBtn].surname
-           tel.value=userDataObj[indexBtn].tel
-           email.value=userDataObj[indexBtn].email
-           saveBtn.style.display = "none";
-           modifayButton.style.display = "block";
-           xButton.style.display = "block";
-        })
-    })
+  const editsButtons = document.querySelectorAll(".edit");
+  editsButtons.forEach((item, indexBtn) => {
+    item.addEventListener("click", () => {
+      index = indexBtn;
+      name.value = userDataObj[indexBtn].name;
+      surname.value = userDataObj[indexBtn].surname;
+      tel.value = userDataObj[indexBtn].tel;
+      email.value = userDataObj[indexBtn].email;
+      saveBtn.style.display = "none";
+      modifayButton.style.display = "block";
+      xButton.style.display = "block";
+    });
+  });
 }
 
 function editUserData() {
-    userDataObj[index]={
-        name:name.value,
-        surname:surname.value,
-        tel:tel.value,
-        email:email.value
-    }
-    createUserTable(userDataObj)
+  userDataObj[index] = {
+    name: name.value,
+    surname: surname.value,
+    tel: tel.value,
+    email: email.value,
+  };
+  createUserTable(userDataObj);
 }
-modifayButton.addEventListener("click",()=>{
-    editUserData()
-})
+modifayButton.addEventListener("click", () => {
+  editUserData();
+});
 xButton.addEventListener("click", () => {
-    modifayButton.style.display = "none";
-    x.style.display = "none";
-    saveBtn.style.display = "block";
-  });
+  modifayButton.style.display = "none";
+  x.style.display = "none";
+  saveBtn.style.display = "block";
+});
 function remove() {
-    const removeBtn = document.querySelectorAll(".remove")
-    removeBtn.forEach((item,indexBtn)=>{
-        item.addEventListener("click",()=>{
-            index = indexBtn
-            userDataObj = userDataObj.filter((i, idx) => index !== idx);
-            createUserTable(userDataObj)
-        })
-    })
+  const removeBtn = document.querySelectorAll(".remove");
+  removeBtn.forEach((item, indexBtn) => {
+    item.addEventListener("click", () => {
+      index = indexBtn;
+      userDataObj = userDataObj.filter((i, idx) => index !== idx);
+      createUserTable(userDataObj);
+    });
+  });
 }
 
-function serach() {
-    const search = document.querySelector("#search");
+const search = document.querySelector("#search");
 
-    search.addEventListener("input",(e)=>{
-        const value = e.target.value
-        let usernew=[]
-        let newarr=userDataObj.map((user)=>{
-            if (user.name === value) {
-                usernew.push(user)
-                createUserTable(usernew)
-            }
-            else{createUserTable(userDataObj);}
-        })
-        console.log(newarr);
-    })
-
-}
-serach()
+search.addEventListener("input", (e) => {
+  const value = e.target.value;
+  let usernew = [];
+  let newarr = userDataObj.map((user) => {
+    if (user.name.includes(value)) {
+      usernew.push(user);
+      createUserTable(usernew);
+    }
+  });
+});
